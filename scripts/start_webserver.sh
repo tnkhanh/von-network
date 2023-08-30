edb20ceb8c5d8dbd6182c64f5982d9d7ab8c7809
TN Khanh <TN Khanh>
2023-08-30 07:48:03 +0000

    Add Trustee seed

--- scripts/start_webserver.sh
+++ scripts/start_webserver.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+export GENESIS_FILE=/home/indy/config/pool_transactions_genesis
+export LEDGER_SEED=000000000000000000000000Trustee1
+
 if [ ! -f "/home/indy/ledger/sandbox/pool_transactions_genesis" ] && [ -z "${GENESIS_URL}" ] && [ -z "${GENESIS_FILE}" ]; then
   echo "Ledger does not exist - Creating genesis data..."
   bash ./scripts/init_genesis.sh
